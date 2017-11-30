8599b30ad0c07e04f261221091cbb2a135b60803
ElenaStessel <olenikovaelena@gmail.com>
2017-11-30 20:40:00 +0300

    Init

---
+++ send-email.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+$username = 'USERNAME';
+$password = 'PASSWORD';
+$sentTo = 'SENDTO@gmail.com';
+
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+  ->setUsername($username)
+  ->setPassword($password);
+
+$mailer = new Swift_Mailer($transport);
+
+$message = (new Swift_Message('Письмо'))
+  ->setFrom($username)
+  ->setTo($sentTo)
+  ->setBody('Телефон: ' . $phone . '. Email: ' . $email);
+
+try {
+$result = $mailer->send($message);
+} catch (\Exception $e) {
+    var_dump($e);
+    die;
+}
+
+header('Location: /');
